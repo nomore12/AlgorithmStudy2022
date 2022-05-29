29c1cc964d7bc9e71b901867953d64f1b2fa6d85
노성호 <nightwing@naver.com>
2022-05-29 14:12:42 +0900

    10/10

--- Programmers/NOMORE/12926.js
+++ Programmers/NOMORE/12926.js
@@ -1,6 +1,18 @@
 function solution(s, n) {
-  var answer = '';
   const lower = 'abcdefghijklmnopqrstuvwxyz'
   const upper = lower.toUpperCase();
-  return answer;
+  const ascArr = s.split('').map((item) => {
+    if (item === ' ') return item;
+    const ss = lower.includes(item) ? lower : upper;
+    const index = ss.indexOf(item);
+    const replace = index + n >= ss.length ?
+      index + n - ss.length :
+      index + n;
+    const char = ss[replace];
+    return char;
+  })
+
+  return ascArr.join('');
 }
+
+console.log(solution('AXYZ', 3));
